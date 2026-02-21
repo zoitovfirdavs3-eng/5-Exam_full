c978fc1f87f8afe5d6b51faa0f0e0dbf60817e05
zoitovfirdavs3-eng <zoitovfirdavs3@gmail.com>
2026-02-21 11:49:39 +0500

    Add production

--- src/lib/db.service.js
+++ src/lib/db.service.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 async function dbConnection(){
     try{
-        // await mongoose.connect(process.env.MONGO_URI);
-        await mongoose.connect(`mongodb://127.0.0.1:27017/exam`)
+        await mongoose.connect(process.env.MONGO_URI);
+        // await mongoose.connect(`mongodb://127.0.0.1:27017/exam`)
         console.log("DB successfully connected ✅")
     }catch(err){
         console.log(`DB connection error: ${err.message}`);
